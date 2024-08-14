96606de794923df3fef0d938e808068c5a788f64
L33gn21 <l33gn21@gmail.com>
2024-08-14 20:44:29 +0900

    프론트엔드 ai로 생성 index

--- src/main/generated/com/myspring/springmaster/dataAccess/mapper/HouseMapperImpl.java
+++ src/main/generated/com/myspring/springmaster/dataAccess/mapper/HouseMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-13T21:58:00+0900",
+    date = "2024-08-14T20:43:49+0900",
     comments = "version: 1.5.3.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-8.7.jar, environment: Java 21.0.2 (Eclipse Adoptium)"
 )
 @Component
