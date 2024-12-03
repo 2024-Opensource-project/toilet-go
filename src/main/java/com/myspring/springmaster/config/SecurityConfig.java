630f6f876effe964c179971d6756050decb781b7
hyun <130212376+Kyunghk@users.noreply.github.com>
2024-12-03 18:39:18 +0900

    소셜 로그인 구글 완료

--- src/main/java/com/myspring/springmaster/config/SecurityConfig.java
+++ src/main/java/com/myspring/springmaster/config/SecurityConfig.java
@@ -1,13 +1,12 @@
 package com.myspring.springmaster.config;
 
 import com.myspring.springmaster.service.CustomOAuth2UserService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -35,28 +34,15 @@ public class SecurityConfig {
                         .loginPage("/signin")        // 로그인 폼 경로
                         .loginProcessingUrl("/signin/authenticate")  // 로그인 인증 처리 URL을 별도로 설정
                         .defaultSuccessUrl("/", true)
-                        .successHandler(customSuccessHandler())
                         .permitAll()
                 )
                 .oauth2Login(oauth2Login -> oauth2Login
                         .loginPage("/signin")
                         .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
-                        .successHandler(customSuccessHandler())  // 소셜 로그인 성공 시 리디렉션할 페이지 설정
+                        .defaultSuccessUrl("/", true)// 소셜 로그인 성공 시 리디렉션할 페이지 설정
                 );
 
         return http.build();
-    }
-
-    @Bean
-    public AuthenticationSuccessHandler customSuccessHandler() {
-        return new AuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                                org.springframework.security.core.Authentication authentication) throws IOException, ServletException {
-                // 여기에서 추가적인 로직 수행 가능
-                response.sendRedirect("/");  // 홈 화면 대신 다른 경로로 리다이렉트할 수 있음
-            }
-        };
     }
 
 }
